a3d912597e0b2290ec87e5cbe8d07ec9408c51c8
ihyun <okyee8335@naver.com>
2022-12-10 22:16:14 +0900

    Feat..ing)Seperate socket

---
+++ routes/socket.router.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router;
+
+const socket = require('../socket');
+
+router.get('/socket', socket);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,65 +1,26 @@
-const path = require('path');
-const http = require('http');
+require('dotenv').config();
+const helmet = require('helmet');
 const express = require('express');
-const socketio = require('socket.io');
-const formatMessage = require('./utils/messages.js')
-const {userJoin, getCurrentUser, userLeave, getRoomUsers} = require('./utils/users.js')
-
+const http = require('http');
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+const routes = require('./routes');
+const socket = require('./socket');
+socket(server)
 
-//Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+const cors = require('cors');
+//const connect = require('./schema');
+//connect();
 
-//Run when a client connects
-io.on('connection', socket => {
-    //Three ways to send a messages
-    socket.on('joinRoom', ({username, room}) => {
-        //Actually create a user
-        const user = userJoin(socket.id, username, room);
-        socket.join(user.room);
+app.use(helmet());
+app.use(express.json());
+app.use(cors({
+    origin: '*',
+}))
 
-        //Welcome current user
-        socket.emit('message', formatMessage('Admin', 'Welcome to ChatCord!'));  //①Only send a message to a single client who connect
+app.use('/', routes);
 
-        //Broadcast when a user connects
-        socket.broadcast
-            .to(user.room)  //Emit to a specific room
-            .emit('message', formatMessage('Admin', `${user.username} has joined the chat`));  //②Emit messages to everybody expect the user that's connecting
-
-        //Send users and room info
-        io.to(user.room).emit('roomUsers', {
-            room: user.room,
-            users: getRoomUsers(user.room)
-        });
-    });
-
-    //Listen for chatMessage
-    socket.on('chatMessage', (msg) => {
-        const user = getCurrentUser(socket.id);
-
-        io.to(user.room).emit('message', formatMessage(user.username, msg));
-    });
-
-    //Runs when client disconnects
-    socket.on('disconnect', () => {
-        const user = userLeave(socket.id);
-
-        if (user) {
-            io.to(user.room).emit('message', formatMessage('Admin', `${user.username} has left the chat`));  //③Emit messages to all the clients in general
-
-            //Send users and room info
-            io.to(user.room).emit('roomUsers', {
-                room: user.room,
-                users: getRoomUsers(user.room)
-            });
-        }
-    });
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+    console.log(`${port}번 포트 오픈~~~~!!!!`);
 });
-
-
-const port = process.env.PORT || 3005;
-
-server.listen(port, () => console.log(`Server running on port ${port}`));
-
