a21742418c5f373c3179908bf01306b8eeb876e9
ihyun <97588819+JadeBeck@users.noreply.github.com>
2023-01-14 14:35:34 +0900

    Delete generalErrorHandler.js

--- src/error/generalErrorHandler.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const generalErrorHandler = (err, req, res, next) => {
-    const { message, statusCode } = err;
-    //console.error(err);
-    res.status(statusCode || 500).json({ message });
-};
-exports.default = generalErrorHandler;
